6be5587251b8576b1e0065ffc53d5daccd445b86
bbggkkk <bbggkkk@naver.com>
2021-10-12 23:18:23 +0900

    수정

--- js/start.js
+++ js/start.js
@@ -1,10 +1,12 @@
 (function(){
     const element = document.querySelectorAll('[data-scroll-animation]');
     for(let i=0; i<element.length; i++){
-        element[i].scrollAnimation = new ScrollAnimation(element[i],window,element[i].getAttribute('data-scroll-animation'), element[i].getAttribute('data-scroll-start'), element[i].getAttribute('data-scroll-end'));
-
-        element[i].removeAttribute('data-scroll-start');
-        element[i].removeAttribute('data-scroll-end');
+        setTimeout(() => {
+            element[i].scrollAnimation = new ScrollAnimation(element[i],window,element[i].getAttribute('data-scroll-animation'), element[i].getAttribute('data-scroll-start'), element[i].getAttribute('data-scroll-end'));
+    
+            element[i].removeAttribute('data-scroll-start');
+            element[i].removeAttribute('data-scroll-end');
+        },0);
     }
     // [...bodys].forEach(item => { item.ScrollAnimation = new ScrollAnimation(item, window, item.getAttribute('data-scroll-animation')); } );
 })();

fd410bb085c93c8777e74ab8befe2c288a5a97b0
bbggkkk <bbggkkk@naver.com>
2021-11-24 22:32:00 +0900

    수정

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,17 @@ const box = document.querySelector('#box') as HTMLElement;
 // const [updator, getKeyframe] = createKeyframes(getCSSAttribute(box),100);
 // let   animation = updator();
 
+const dragFunction = (r:any, e:TouchEvent|MouseEvent, origin:any) => {
+    let [originX, originY] = origin === undefined ? [0, 0] : origin;
+    const [x, y]   = r.move;
+    [originX, originY] = [originX+x, originY+y];
+    gotoAndStop(box, [{transform:`translate(calc(${originX}px), calc(${originY}px))`}], 0);
+    return [originX, originY];
+}
+
 const [on, off] = createGesture(box, {
-    dragStart : async (r:any, e:TouchEvent|MouseEvent) => {
-        const [x, y] = r.position;
-        gotoAndStop(box, [{transform:`translate(calc(-50% + ${x}px), calc(-50% + ${y}px))`}], 0);
-    },
+    dragStart   : dragFunction,
+    drag        : dragFunction,
+    dragEnd     : dragFunction,
 });
 on();

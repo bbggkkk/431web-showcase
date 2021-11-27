33a81c9b1d4284271ce950aaa66be755d4cc28df
bbggkkk <bbggkkk@naver.com>
2021-11-27 14:40:57 +0900

    수정

--- src/index.ts
+++ src/index.ts
@@ -27,23 +27,31 @@ box.forEach(async $item => {
     const eleList = Array.from(($item as HTMLElement).querySelectorAll(':scope [data-animation-0]'));
     eleList.push($item as HTMLElement);
     const aniList = eleList.map((item:HTMLElement) => {
-        const tmp = createKeyframes(getCSSAttribute(item as HTMLElement), () => Math.round(document.documentElement.offsetHeight));
+        const tmp = createKeyframes(getCSSAttribute(item as HTMLElement), () => Math.round(document.documentElement.offsetHeight/2));
         gotoAndStop(item as HTMLElement, [tmp[1](0)], 0);
         return tmp;
     });
     let anis = [];
-    aniList.forEach(async item => anis.push(await item[0]()) );
+    aniList.forEach(async (item, idx) => {
+        const ob = new ResizeObserver(async () => {
+            anis[idx] = await item[0]();
+        });
+        ob.observe(document.documentElement);
+    } );
 
     const [on, off] = createGesture($item as HTMLElement, {
         dragStart   : function(r:any, e:TouchEvent|MouseEvent, origin:any) {
-            // console.log(anis);
             anis.forEach((item, idx:number) => {
                 let [originX, originY] = origin === undefined ? [0, 0] : origin;
                 const [x, y]   = r.move;
                 [originX, originY] = [originX+x, originY+y];
-                if(Math.abs(Math.round(originY)) >= item.length || Math.round(originY) > 0) return;
-                // console.log(Math.round(originY),item[Math.round(originY)]);
-                gotoAndStop(eleList[idx] as HTMLElement, item as any, Math.abs(Math.round(originY)));
+                if(Math.round(originY) > 0){
+                    gotoAndStop(eleList[idx] as HTMLElement, item as any, 0);
+                }else if(Math.abs(Math.round(originY)) > item.length){
+                    gotoAndStop(eleList[idx] as HTMLElement, item as any, item.length - 1);
+                }else{
+                    gotoAndStop(eleList[idx] as HTMLElement, item as any, Math.abs(Math.round(originY)));
+                }
             });
             return dragFunction.call(this, r, e, origin);
         },
@@ -52,9 +60,13 @@ box.forEach(async $item => {
                 let [originX, originY] = origin === undefined ? [0, 0] : origin;
                 const [x, y]   = r.move;
                 [originX, originY] = [originX+x, originY+y];
-                if(Math.abs(Math.round(originY)) >= item.length || Math.round(originY) > 0) return;
-                // console.log(Math.round(originY),item[Math.round(originY)]);
-                gotoAndStop(eleList[idx] as HTMLElement, item as any, Math.abs(Math.round(originY)));
+                if(Math.round(originY) > 0){
+                    gotoAndStop(eleList[idx] as HTMLElement, item as any, 0);
+                }else if(Math.abs(Math.round(originY)) > item.length){
+                    gotoAndStop(eleList[idx] as HTMLElement, item as any, item.length - 1);
+                }else{
+                    gotoAndStop(eleList[idx] as HTMLElement, item as any, Math.abs(Math.round(originY)));
+                }
             });
             return dragFunction.call(this, r, e, origin);
         },

a49952d38d22cded20a0481ba6cdc34992e0bdb4
bbggkkk <bbggkkk@naver.com>
2022-01-11 20:42:36 +0900

    메인 타이틀 수정

--- src/components/main/Title.tsx
+++ src/components/main/Title.tsx
@@ -10,59 +10,82 @@ const PlaceHolder = styled.div`
 `
 const TitleComponent = styled.div`
     width:100%;
-    height:150vh;
-    position:absolute;
-    top:0;
-
-    .title-wrap {
+    
+    .main-title {
         width:100%;
-        height:calc(100% / 3 * 2);
-        box-sizing:border-box;
-
-        position:sticky;
+        height:150vh;
+        position:absolute;
         top:0;
-
-        .title-box {
+        
+        .title-wrap {
             width:100%;
-            height:100%;
-            overflow:hidden;
-
-            .content-box {
-                padding:32px;
-                position:absolute;
-                top:0; left:0; right:0; bottom:0;
+            height:calc(100% / 3 * 2);
+            box-sizing:border-box;
+    
+            position:sticky;
+            top:0;
+    
+            .title-box {
+                width:100%;
+                height:100%;
+                overflow:hidden;
+    
+                .content-box {
+                    padding:32px;
+                    position:absolute;
+                    top:0; left:0; right:0; bottom:0;
+    
+                    h1 {
+                        position:absolute;
+                        top:25%;
+                        left:50%;
+                        transform:translate(-50%, -50%);
+                        font-size:${theme.typo['size-title-1']};
+                        font-weight:${theme.typo['weight-black']};
+                        white-space:nowrap;
+                    }
+                }
             }
         }
     }
+
 `
 
 function Title() {
     const target = useRef();
     useEffect(()=>{
-        const scrollAnimation = new ScrollAnimation(window, '.main-title .box')
+        const scrollAnimation = new ScrollAnimation(window, '.main-wrap .box')
     },[]);
     return (
-        <>
+        <TitleComponent className="main-wrap">
             <PlaceHolder/>
-            <TitleComponent className="main-title">
+            <div className="main-title box"
+                        data-animation-start="<$ return parseInt(window.outerHeight/1.5); $>"
+                        data-animation-end="<$ return parseInt(window.outerHeight*1.5); $>"
+                        data-animation-0="transform:scale(1) translate(0%, 0%); opacity:1;"
+                        data-animation-50="opacity:1;"
+                        data-animation-100="transform:scale(0.2) translate(100%, 100%); opacity:0;">
                 <div    className='title-wrap box' ref={target}
                         data-animation-start="0"
-                        data-animation-end="<$ return parseInt(window.innerHeight/2); $>"
+                        data-animation-end="<$ return parseInt(window.outerHeight/2); $>"
                         data-animation-0="padding:0px;"
                         data-animation-100="padding:8px;">
                     <div    className='title-box box'
                             data-animation-start="0"
-                            data-animation-end="<$ return parseInt(window.innerHeight/2); $>"
+                            data-animation-end="<$ return parseInt(window.outerHeight/2); $>"
                             data-animation-0="border-radius:0px;"
                             data-animation-100="border-radius:48px;">
-                        <img src="https://images.unsplash.com/photo-1641765770550-e2a1e9a61082?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8" style={{width:'100%',height:'100%',objectFit:'cover'}} alt="title"/>
+                        <img src="https://images.unsplash.com/photo-1641763773957-35922086117c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" style={{width:'100%',height:'100%',objectFit:'cover'}} alt="title"/>
                         <div className="content-box">
-                            <h1 style={{fontWeight:900}}>431Web</h1>
+                            <h1>
+                                <p>431Web</p>
+                                <p>포트폴리오 입니다.</p>
+                            </h1>
                         </div>
                     </div>
                 </div>
-            </TitleComponent>
-        </>
+            </div>
+        </TitleComponent>
     );
 }
 
